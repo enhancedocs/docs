7fe664d1723826121d33d8c8cd5cd3ca56740f5b
Alw3ys <alw3ys@gmail.com>
2023-03-28 18:11:07 +0200

    implement enhancedocs search

---
+++ src/theme/SearchBar.js
@@ -0,0 +1,18 @@
+/**
+ * @see https://docusaurus.io/docs/search#using-your-own-search
+ */
+
+import React from 'react';
+import EnhancedSearch from 'enhancedocs-search';
+
+import 'enhancedocs-search/dist/style.css';
+
+export default function SearchBarWrapper(props) {
+  return (
+    <EnhancedSearch
+      accessToken="pk_760ed6e637a6ab987495e43916e7fc2799de0c074e2c1d86"
+      placeholder="Search"
+      {...props}
+    />
+  );
+}
