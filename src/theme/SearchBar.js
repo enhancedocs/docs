955f5dbe3770cc17e786540392327aaeb685ac00
playolaizq <pol944li@gmail.com>
2023-05-23 08:21:51 +0200

    core: enhancedocs-search v1.0.0

--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -11,10 +11,8 @@ export default function SearchBarWrapper(props) {
   return (
     <EnhancedSearch
       config={{
-        enhancedSearch: {
-          projectId: '646a4e6158de6ee4ab3076cd',
-          accessToken: 'pk_919329ca3f52aeae4ba58aa1021f4a63eba7454d54ca86c7'
-        }
+        projectId: '646a4e6158de6ee4ab3076cd',
+        accessToken: 'pk_919329ca3f52aeae4ba58aa1021f4a63eba7454d54ca86c7'
       }}
       {...props}
     />
